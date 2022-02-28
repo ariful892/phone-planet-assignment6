f3db35b4ec2a4fd0313c197dfac5e7f2df644798
ariful892 <ariful00892@gmail.com>
2022-03-01 02:09:00 +0600

    js search result

--- js/mobile-planet.js
+++ js/mobile-planet.js
@@ -12,5 +12,21 @@ document.getElementById('search-btn').addEventListener('click', function () {
 });
 
 const showSearchResult = phones => {
-    console.log(phones);
+
+    const searchResult = document.getElementById('search-result');
+    for (const phone of phones) {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card mx-3">
+            <img  src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+             <h5 class="card-title">Card title</h5>
+                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                to additional content. This content is a little bit longer.</p>
+             </div>
+        </div>`;
+        searchResult.appendChild(div);
+    }
 }
